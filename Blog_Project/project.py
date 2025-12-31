a1b7f0b48cad358428b6874cf9adca0d26a8b682
Dineshdini71 <dinidinesh71@gmail.com>
2025-12-31 18:43:45 +0530

    Added year on copyright.

--- Blog_Project/project.py
+++ Blog_Project/project.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 import requests
 import smtplib as st
 import os
 from dotenv import load_dotenv
-# from blog import Blog
 
 load_dotenv()
 blog_pass = os.environ.get('blog_post')
-
+current_year = datetime.now().year
 blog_url = "https://api.npoint.io/955c1e57d6bfa98e22ae"
 blog_ = requests.get(blog_url)
 blog_res = blog_.json()
-
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", blogs = blog_res)
+    return render_template("index.html", blogs = blog_res, year=current_year)
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", year=current_year)
 
 @app.route("/login", methods=['POST', 'GET'])
 def data_received():
@@ -40,12 +39,12 @@ def data_received():
                 to_addrs= email,
                 msg= message
             )
-        return render_template("login.html", name=name, email=email, mssg=message, phone=ph , msg_sent=True)
-    return render_template("login.html", msg_sent=False)
+        return render_template("login.html", name=name, email=email, mssg=message, phone=ph , msg_sent=True, )
+    return render_template("login.html", msg_sent=False, year=current_year)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", year=current_year)
 
 @app.route('/post/<int:index>')
 def show_post(index):
@@ -53,7 +52,7 @@ def show_post(index):
     for blog_post in blog_res:
         if blog_post["id"] == index:
             requested_post = blog_post
-    return render_template("post.html", num=requested_post)
+    return render_template("post.html", num=requested_post, year=current_year)
 
 if __name__ == '__main__':
     app.run(debug=True)
